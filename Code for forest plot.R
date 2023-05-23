fc1c3bdfee9635670305fc81c3bcb572e1b31a9e
LilyAndrews6 <57757219+LilyAndrews6@users.noreply.github.com>
2023-05-23 15:01:29 +0100

    Update Code for forest plot.R

--- Code for forest plot.R
+++ Code for forest plot.R
@@ -1,6 +1,9 @@
 #Random effects model and freeman-tukey double arcsine transformation
 install.packages("meta")
 library("meta")
+#Read in data
+wadata <- read.table("data.txt, header=T)
+
 #Normal model - for a standard forest plot
 w <- metaprop(wadata$`V600 mutation prevalence`, wadata$`Total sample size (n)`, wadata$Author, #Change this line to your variables
          data = NULL, subset = NULL,
@@ -11,6 +14,7 @@ w <- metaprop(wadata$`V600 mutation prevalence`, wadata$`Total sample size (n)`,
          title="", complab="", outclab="",
          print.byvar=TRUE,
          warn=TRUE)
+
 #Model by variable - to create a forest plot into different variables
 w <- metaprop(wadata$`V600 mutation prevalence`, wadata$`Total sample size (n)`, wadata$Author, #Change this line to your variables
               data = NULL, subset = NULL,
@@ -21,6 +25,7 @@ w <- metaprop(wadata$`V600 mutation prevalence`, wadata$`Total sample size (n)`,
               title="", complab="", outclab="",
               print.byvar=TRUE,
               warn=TRUE)
+
 #Create forest plot and adjust to what you would like 
 png(file='subtype_plot.png', width=800, height=15000) #These can be adjusted accordingly, this setting is to download a large plot
 forest(w,
